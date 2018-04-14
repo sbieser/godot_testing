c70a7b6dc7e8e54baa0327a58a0b561da667cfa6
scott <bieser.scott@gmail.com>
2018-04-13 22:31:25 -0700

    nailed down the player movement. Just need a little more work on the player attack movement. Also added new animation for jumping. Resolved some other animation flickering with jumping from a crouched state

--- Player.gd
+++ Player.gd
@@ -11,29 +11,21 @@ export (int) var run_speed = 100
 export (int) var walk_speed = 85
 
 #TODO: We need to have states to show which direction the player is facing, left or right
-enum {IDLE, JUMP, ATTACK, WALK, ROLL, RUN, CROUCH, CROUCH_WALK, STUNNED, STUNNED_IDLE}
+enum {IDLE, JUMP, ATTACK, WALK, CROUCH_IDLE, CROUCH_WALK, CROUCH_ATTACK}
 enum {RIGHT_FACING, LEFT_FACING}
 var velocity = Vector2()
 var state
 var facing
-var crouching
-var attacking = false
 var anim
 var new_anim
 
 var hit_bodies = [] #array of bodies bit by the current attack
 
-
-#new better variables?
-#var prev_velocity #not working very well, will have to come back to this
-var cur_speed
-
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
-	facing = RIGHT_FACING
 	$AttackArea.visible = false
-	crouching = false
+	facing = RIGHT_FACING
 	change_state(IDLE)
 
 func change_direction():
@@ -43,48 +35,33 @@ func change_direction():
 		facing = RIGHT_FACING
 
 func change_state(new_state):
-	#print(str(new_state))
-	$AttackArea.visible = false
-	state = new_state
-	match state:
-		IDLE:
-			new_anim = "idle"
-			crouching = false
-			attacking = false
-		ATTACK:
-			if crouching:
-				new_anim = "crouch_attack"
-			else:
+	if new_state != state:
+		state = new_state
+		match state:
+			IDLE:
+				new_anim = "idle"
+			CROUCH_IDLE:
+				new_anim = "crouch"
+			WALK:
+				new_anim = "walk"
+			CROUCH_WALK:
+				new_anim = "crouch_walk"
+			ATTACK:
 				new_anim = "attack"
-			attacking = true
-			$AttackArea.visible = true
-			get_node("AttackArea/AttackTimer").start()
-		WALK:
-			new_anim = "walk"
-			crouching = false
-			change_direction()
-		CROUCH:
-			new_anim = "crouch"     
-			crouching = true
-		CROUCH_WALK:
-			new_anim = "crouch_walk"
-			crouching = true
-			change_direction()
-		STUNNED:
-			#some stunned animation or something should play
-			pass
-		STUNNED_IDLE:
-			velocity.x = 0
-			$StunIdleTimer.start()
-		RUN:
-			new_anim = "run"
-			change_direction()
-		ROLL:
-			new_anim = "roll"
-			$RollTimer.start()
-			change_direction()
+				$AttackArea.visible = true
+				get_node("AttackArea/AttackTimer").start()
+			CROUCH_ATTACK:
+				new_anim = "crouch_attack"
+				$AttackArea.visible = true
+				get_node("AttackArea/AttackTimer").start()
+			JUMP:
+				new_anim = "jump"
 	
-	if crouching == true:
+	if state == WALK or state == CROUCH_WALK:
+		change_direction()
+	
+	#change player size based on standing or crouching
+	if state == CROUCH_IDLE or state == CROUCH_WALK or state == CROUCH_ATTACK:
 		$CollisionShape2D.shape.set_extents(Vector2(16,16))
 		$CollisionShape2D.position = Vector2(0,16)
 		$AnimatedSprite.position = Vector2(0,16)
@@ -95,147 +72,174 @@ func change_state(new_state):
 	
 	if facing == LEFT_FACING:
 		$AnimatedSprite.flip_h = true
-		if crouching:
+		if state == CROUCH_WALK or state == CROUCH_IDLE or state == CROUCH_ATTACK:
 			get_node("AttackArea/CollisionShape2D").position = Vector2(-33,16)
 		else:
 			get_node("AttackArea/CollisionShape2D").position = Vector2(-33,-16)
 	else:
 		$AnimatedSprite.flip_h = false
-		if crouching:
+		if state == CROUCH_WALK or state == CROUCH_IDLE or state == CROUCH_ATTACK:
 			get_node("AttackArea/CollisionShape2D").position = Vector2(33,16)
 		else:
 			get_node("AttackArea/CollisionShape2D").position = Vector2(33,-16)
-			
+	
+	#$AttackArea.visible = false
+	#state = new_state
+	#match state:
+#		IDLE:
+#			new_anim = "idle"
+#			crouching = false
+#			attacking = false
+#		ATTACK:
+#			if crouching:
+#				new_anim = "crouch_attack"
+#			else:
+#				new_anim = "attack"
+#			attacking = true
+#			$AttackArea.visible = true
+#			get_node("AttackArea/AttackTimer").start()
+#		WALK:
+#			new_anim = "walk"
+#			crouching = false
+#			change_direction()
+#		CROUCH:
+#			new_anim = "crouch"     
+#			crouching = true
+#		CROUCH_WALK:
+#			new_anim = "crouch_walk"
+#			crouching = true
+#			change_direction()
+#		STUNNED:
+#			#some stunned animation or something should play
+#			pass
+#		STUNNED_IDLE:
+#			velocity.x = 0
+#			$StunIdleTimer.start()
+#		RUN:
+#			new_anim = "run"
+#			change_direction()
+#		ROLL:
+#			new_anim = "roll"
+#			$RollTimer.start()
+#			change_direction()
+#	
+#	if crouching == true:
+#		$CollisionShape2D.shape.set_extents(Vector2(16,16))
+#		$CollisionShape2D.position = Vector2(0,16)
+#		$AnimatedSprite.position = Vector2(0,16)
+#	else:
+#		$CollisionShape2D.shape.set_extents(Vector2(16,32))
+#		$CollisionShape2D.position = Vector2(0,0)
+#		$AnimatedSprite.position = Vector2(0,0)
 
+func handle_horizontal_movement():
+	var right = Input.is_action_pressed("ui_right") #movement right
+	var left = Input.is_action_pressed("ui_left") # movement left
+	var down = Input.is_action_pressed("ui_down") # crouch button
+	
+	var cur_speed = walk_speed
+	if down:
+		cur_speed = walk_speed / 2
+	if right:
+		velocity.x += cur_speed
+	if left:
+		velocity.x -= cur_speed
 
-
+func handle_action():
+	pass
+	
+	
+func print_state():
+	#enum {IDLE, JUMP, ATTACK, WALK, CROUCH_IDLE, CROUCH_WALK, CROUCH_ATTACK}
+	match state:
+		IDLE:
+			print("IDLE")
+		JUMP:
+			print("JUMP")
+		ATTACK:
+			print("ATTACK")
+		WALK:
+			print("WALK")
+		CROUCH_IDLE:
+			print("CROUCH_IDLE")
+		CROUCH_WALK:
+			print("CROUCH_WALK")
+		CROUCH_ATTACK:
+			print("CROUCH_ATTAK")
 
 func handle_input():
-	print(str(state))
-	#set velocity to 0 initially
-	velocity.x = 0
+	print_state()
+	
+	if state == ATTACK or state == CROUCH_ATTACK:
+		return
 	
 	#Left/Right - Move
 	#Up         - Look up / Climb
 	#Down       - Look down / Crouch / Climb / Run (If down-for-running is enabled)
 	#X          - Action
 	#Z          - Jump
-	#C          - Cycle held item, Bombs, Rope
-	#Shift      - Run
-	var right = Input.is_action_pressed("ui_right")
-	var left = Input.is_action_pressed("ui_left")
-	var up = Input.is_action_pressed("ui_up")
-	var down = Input.is_action_pressed("ui_down")
-	var x = Input.is_action_pressed("ui_x") # 
-	var z = Input.is_action_pressed("ui_z") # a button for run
-	var shift = Input.is_action_pressed("ui_shift")
+	var right = Input.is_action_pressed("ui_right") #movement right
+	var left = Input.is_action_pressed("ui_left") # movement left
+	var down = Input.is_action_pressed("ui_down") # crouch button
+	var x = Input.is_action_pressed("ui_x") # action / attack button
+	var z = Input.is_action_pressed("ui_z") # jump button
+	
+	#set velocity to 0 initially
+	velocity.x = 0
+	handle_horizontal_movement() #find horizontal movememnt through left and right inputs
 	
 	match state:
 		IDLE:
-			if right:
-				velocity.x += cur_speed
-				state = WALK
-				#change_direction()
-				#change_state(WALK)
-			if left:
-				velocity.x -= cur_speed
-				state = WALK
-				#change_direction()
-				#change_state(WALK)
-			if z:
-				state = JUMP 
+			if velocity.x != 0:
+				change_state(WALK)
+			elif z and is_on_floor():
 				velocity.y = jump_speed
-		WALK:
-			if shift:
-				state = RUN
-				#continue
-			if right:
-				velocity.x += cur_speed
-			if left:
-				velocity.x -= cur_speed
-			if z:
-				state = JUMP
+				change_state(JUMP)
+			elif down:
+				change_state(CROUCH_IDLE)
+			elif x:
+				change_state(ATTACK)
+		CROUCH_IDLE:
+			if velocity.x != 0:
+				change_state(CROUCH_WALK)
+			elif x:
+				change_state(CROUCH_ATTACK)
+			elif z and is_on_floor():
 				velocity.y = jump_speed
-			if velocity.x == 0:
-				state = IDLE
-		JUMP:
-			if right:
-				velocity.x += cur_speed
-			elif left:
-				velocity.x -= cur_speed
-		RUN:
-			if right:
-				velocity.x += cur_speed
-			if left:
-				velocity.x -= cur_speed
-			if !shift:
-				#state = WALK
+				change_state(JUMP)
+			elif not down:
 				if velocity.x != 0:
-					state = WALK
+					change_state(WALK)
 				else:
-					state = IDLE
-			if z:
-				state = JUMP
+					change_state(IDLE)
+		CROUCH_WALK:
+			if not down:
+				if velocity.x != 0:
+					change_state(WALK)
+				else:
+					change_state(IDLE)
+			elif velocity.x == 0:
+				change_state(CROUCH_IDLE)
+			elif z and is_on_floor():
 				velocity.y = jump_speed
-	
-func get_input():
-	if attacking || state == STUNNED || state == STUNNED_IDLE:
-		return
-	
-	velocity.x = 0
-	var right = Input.is_action_pressed("ui_right")
-	var left = Input.is_action_pressed("ui_left")
-	var jump = Input.is_action_just_pressed("ui_select")
-	var attack = Input.is_action_just_pressed("ui_attack")
-	var down = Input.is_action_pressed("ui_down")
-	var run = Input.is_action_pressed("ui_run")
-	var block = Input.is_action_pressed("ui_block")
-	
-	if not attacking and attack:
-		change_state(ATTACK)
-		return
-		
-	if attack:
-		change_state(ATTACK)
-		return
-	if jump and is_on_floor():
-		change_state(JUMP)
-		velocity.y = jump_speed
-	
-	var speed = walk_speed
-	if run:
-		speed = run_speed
-	
-	if right:
-		velocity.x += speed
-	if left:
-		velocity.x -= speed
-	
-	if down:
-		if velocity.x != 0:
-			change_state(CROUCH_WALK)
-		else:
-			change_state(CROUCH)
-	elif velocity.x != 0:
-		if run and is_on_floor():
-			if state != RUN and state != ROLL:
-				change_state(ROLL)
-		else:
-			change_state(WALK)
-	elif state == WALK or state == CROUCH or state == CROUCH_WALK or state == RUN:
-		change_state(IDLE)
-	
-func handle_speed():
-	var shift = Input.is_action_pressed("ui_shift")
-	
-	if shift:
-		cur_speed = run_speed
-	else:
-		cur_speed = walk_speed
+				change_state(JUMP)
+		WALK:
+			if down:
+				if velocity.x != 0:
+					change_state(CROUCH_WALK)
+				else:
+					change_state(CROUCH_IDLE)
+			elif z and is_on_floor():
+				velocity.y = jump_speed
+				change_state(JUMP)
+			elif velocity.x == 0:
+				change_state(IDLE)
+		JUMP:
+			pass
+		ATTACK:
+			pass
 	
 func _process(delta):
-	#get_input()
-	handle_speed()
 	handle_input()
 	if new_anim != anim:
 		anim = new_anim
@@ -243,11 +247,15 @@ func _process(delta):
 	
 func _physics_process(delta):
 	velocity.y += gravity * delta
+	velocity = move_and_slide(velocity, Vector2(0,-1))
 	
 	match state:
 		JUMP:
 			if is_on_floor():
-				state = IDLE
+				if velocity.x != 0:
+					change_state(WALK)
+				else:
+					change_state(IDLE)
 	
 	#if attacking:
 	#	var bodies = $AttackArea.get_overlapping_bodies()
@@ -277,20 +285,62 @@ func _physics_process(delta):
 	#			velocity.y = jump_speed
 	#			velocity.x = -velocity.x
 	#			
-	velocity = move_and_slide(velocity, Vector2(0,-1))
 	
 func _on_AttackTimer_timeout():
-	hit_bodies.clear()
+	#hit_bodies.clear()
 	get_node("AttackArea/AttackTimer").stop()
+	$AttackArea.visible = false
 	change_state(IDLE)
 
-func _on_StunTimer_timeout():
-	#pass # replace with function body
-	$StunIdleTimer.stop()
-	change_state(IDLE)
 
-func _on_RollTimer_timeout():
-	#pass # replace with function body
-	print("_on_RollTimer_timeout")
-	$RollTimer.stop()
-	change_state(RUN)
+#func _on_StunTimer_timeout():
+#	#pass # replace with function body
+#	$StunIdleTimer.stop()
+#	change_state(IDLE)
+		
+#func get_input():
+#	if attacking || state == STUNNED || state == STUNNED_IDLE:
+#		return
+#
+#	velocity.x = 0
+#	var right = Input.is_action_pressed("ui_right")
+#	var left = Input.is_action_pressed("ui_left")
+#	var jump = Input.is_action_just_pressed("ui_select")
+#	var attack = Input.is_action_just_pressed("ui_attack")
+#	var down = Input.is_action_pressed("ui_down")
+#	var run = Input.is_action_pressed("ui_run")
+#	var block = Input.is_action_pressed("ui_block")
+#
+#	if not attacking and attack:
+#		change_state(ATTACK)
+#		return
+#
+#	if attack:
+#		change_state(ATTACK)
+#		return
+#	if jump and is_on_floor():
+#		change_state(JUMP)
+#		velocity.y = jump_speed
+#
+#	var speed = walk_speed
+#	if run:
+#		speed = run_speed
+#
+#	if right:
+#		velocity.x += speed
+#	if left:
+#		velocity.x -= speed
+#
+#	if down:
+#		if velocity.x != 0:
+#			change_state(CROUCH_WALK)
+#		else:
+#			change_state(CROUCH)
+#	elif velocity.x != 0:
+#		if run and is_on_floor():
+#			if state != RUN and state != ROLL:
+#				change_state(ROLL)
+#		else:
+#			change_state(WALK)
+#	elif state == WALK or state == CROUCH or state == CROUCH_WALK or state == RUN:
+#		change_state(IDLE)
